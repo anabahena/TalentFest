b768f6ef7c234d16875eb575f311da92f734cd8a
anabahena <ana-bahena@live.com.mx>
2019-11-30 16:11:42 -0600

    subiendo modulos con su ruteo completo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'admin', loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule) }, { path: 'login', loadChildren: () => import('./components/auth/login/login.module').then(m => m.LoginModule) }, { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+  { path: '', redirectTo:'/home', pathMatch:'full'},
+  { path: 'admin', loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule) }, 
+  { path: 'login', loadChildren: () => import('./components/auth/login/login.module').then(m => m.LoginModule) }, 
+  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
