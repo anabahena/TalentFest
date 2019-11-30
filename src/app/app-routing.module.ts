ec27b7dc3ce7947567183710f79dd51845912937
anabahena <ana-bahena@live.com.mx>
2019-11-30 15:31:27 -0600

    subiendo componentes y routing de login y pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'admin', loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule) }, { path: 'login', loadChildren: () => import('./components/auth/login/login.module').then(m => m.LoginModule) }, { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
