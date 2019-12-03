4c2679f45e7dc416d35e20da422f68552d3f803f
anabahena <ana-bahena@live.com.mx>
2019-12-03 14:57:13 -0600

    b9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,6 @@ const routes: Routes = [
       { path: 'clients', loadChildren: () => import('./components/pages/clients/clients.module').then(m => m.ClientsModule) },
       { path: 'admin', loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule) },
       { path: 'atcClientes', loadChildren: () => import('./components/pages/atc-clientes/atc-clientes.module').then(m => m.AtcClientesModule) },
-      { path: 'atcClientes', loadChildren: () => import('./components/pages/atc-clientes/atc-clientes.module').then(m => m.AtcClientesModule) },
       { path: 'marketing', loadChildren: () => import('./components/pages/marketing/marketing.module').then(m => m.MarketingModule) },
       { path: 'cobranza', loadChildren: () => import('./components/pages/cobranza/cobranza.module').then(m => m.CobranzaModule) },
     ]
