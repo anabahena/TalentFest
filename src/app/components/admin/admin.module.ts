f8cb8c113b16ce48775bc0ff8c8e3d22fdda69f4
Daynizyvc <zinyad_leay@hotmail.com>
2019-12-03 01:49:51 -0600

    Se agrega la pagina de mensajes

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { MessageFormComponent } from './message-form/message-form.component';
+import { MessagesComponent } from './messages/messages.component';
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, MessageFormComponent, MessagesComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
