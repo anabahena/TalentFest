b86d21244886ae92ecd8dbcb4f8212e14c2f0bc6
anabahena <ana-bahena@live.com.mx>
2019-12-02 21:26:05 -0600

    v8

--- src/app/components/pages/clients/clients.module.ts
+++ src/app/components/pages/clients/clients.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ClientsRoutingModule } from './clients-routing.module';
 import { ClientsComponent } from './clients.component';
 // import {MatTableModule} from '@angular/material/table';
