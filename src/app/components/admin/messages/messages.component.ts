6c5e8e164f623eb60b58fca3e1b72d7b5b38858e
Daynizyvc <zinyad_leay@hotmail.com>
2019-12-03 06:00:58 -0600

    Se actualiza la base de datos en tiempo real, con los mensajes

--- src/app/components/admin/messages/messages.component.ts
+++ src/app/components/admin/messages/messages.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+// importando el servicio
+import { MessageService } from '../../../services/message.service'
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public messageService: MessageService ) { }
 
   ngOnInit() {
+    this.messageService.getMessages().subscribe(messages => {
+      console.log(messages)      
+    })
   }
 
 }
--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,9 +1,24 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+
+// Firestore
+import { AngularFirestore } from '@angular/fire/firestore'
+import { Message } from '../shared/models/message'
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
+  messagesCollection;
+  messages: Observable<Message[]>;
+  messageDoc;
 
-  constructor() { }
+
+  constructor(public db: AngularFirestore) {
+    this.messages = this.db.collection('messages').valueChanges();
+   }
+
+   getMessages(){
+     return this. messages;
+   }
 }
